5219bd0105021a9ec1307210673237b658ed517a
aalexebr <alex.ebr@icloud.com>
2023-09-01 16:30:50 +0200

    ADD genre as array obligatory

--- Movie.php
+++ Movie.php
@@ -3,23 +3,31 @@ class Movie {
     public $original_title;
     public $director;
     private $year_release;
-    public $genre;
+    private $genre;
 
     public function __construct(
         $title,
         $director,
-        $year_released
+        $year_released,
+        $genre 
         )
         {
         $this->original_title = $title;
         $this->director = $director;
         $this->setYear($year_released);
+        $this->setGenre($genre);
     }
     
     public function setYear($num){
         if(is_int($num)){
             $this->year_release = $num;
             return $num;
+        }
+    }
+    public function setGenre($var){
+        if(is_array($var)){
+            $this->year_release = $var;
+            return $var;
         }
     }
 }
--- createMovies.php
+++ createMovies.php
@@ -1,6 +1,5 @@
 <?php
 require_once __DIR__ . './Movie.php';
 
-$newMovie = new Movie("Drive","Refn",2007);
-// $newMovie->original_title = 'drive';
+$newMovie = new Movie("Drive","Refn",2007, ['action']);
 var_dump($newMovie);
